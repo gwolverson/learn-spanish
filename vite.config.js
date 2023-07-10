0ab9dcb5ced124367bf5f5e2f9cd283876659c94
Greg Wolverson <greg.wolverson@capgemini.com>
2023-07-10 20:03:17 +0100

    refactor: vite config for pwa

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { SvelteKitPWA } from '@vite-pwa/sveltekit'
-import { defineConfig } from 'vitest/config';
 
 /** @type {import('vite').UserConfig} */
-export default defineConfig({
-	plugins: [
-		sveltekit(),
-		SvelteKitPWA({})
-	],
+const config = {
+  plugins: [
+    sveltekit(),
+    SvelteKitPWA({/* pwa options */})
+  ],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
-});
+}
+
+export default config
