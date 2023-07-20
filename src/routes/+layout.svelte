b7ea8aca85d75c3e6dbd0b5632107251afa77d35
Greg Wolverson <greg.wolverson@capgemini.com>
2023-07-20 15:44:48 +0100

    fix: manifest and missing config from pwa plugin

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,44 +2,44 @@
 	import '../app.postcss';
 	import Navbar from '../components/layout/Navbar.svelte';
 
-  // import { onMount } from 'svelte'
-  // import { pwaInfo } from 'virtual:pwa-info'; 
+  import { onMount } from 'svelte'
+  import { pwaInfo } from 'virtual:pwa-info'; 
 
-  // onMount(async () => {
-  //   if (pwaInfo) {
-  //     const { registerSW } = await import('virtual:pwa-register')
-  //     registerSW({
-  //       immediate: true,
-  //       onRegistered(r) {
-  //         // uncomment following code if you want check for updates
-  //         // r && setInterval(() => {
-  //         //    console.log('Checking for sw update')
-  //         //    r.update()
-  //         // }, 20000 /* 20s for testing purposes */)
-  //         console.log(`SW Registered: ${r}`)
-  //       },
-  //       onRegisterError(error) {
-  //         console.log('SW registration error', error)
-  //       }
-  //     })
-  //   }
-  // })
+  onMount(async () => {
+    if (pwaInfo) {
+      const { registerSW } = await import('virtual:pwa-register')
+      registerSW({
+        immediate: true,
+        onRegistered(r) {
+          // uncomment following code if you want check for updates
+          // r && setInterval(() => {
+          //    console.log('Checking for sw update')
+          //    r.update()
+          // }, 20000 /* 20s for testing purposes */)
+          console.log(`SW Registered: ${r}`)
+        },
+        onRegisterError(error) {
+          console.log('SW registration error', error)
+        }
+      })
+    }
+  })
 
-  // $: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : '' 
+  $: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : '' 
 </script>
 
-<!-- <svelte:head> 
+<svelte:head> 
  	{@html webManifestLink} 
-</svelte:head> -->
+</svelte:head>
 
 <Navbar />
 <div class="container">
     <slot />
 </div>
 
-<!-- {#await import('$lib/ReloadPrompt.svelte') then { default: ReloadPrompt}}
+{#await import('$lib/ReloadPrompt.svelte') then { default: ReloadPrompt}}
   <ReloadPrompt />
-{/await} -->
+{/await}
 
 <style lang="postcss">
     :global(html) {      
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,69 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit'
 
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [
     sveltekit(),
+    SvelteKitPWA({
+      manifest: {      
+        "name": "Learn Spanish",
+        "short_name": "Learn Spanish",
+        "theme_color": "#AD1519",
+        "background_color": "#FABD00",    
+        "display": "standalone",
+        "scope": "/",
+        "start_url": "/",
+        "id": "/",
+        "icons": [
+          {
+            "src": "icon-48.png",
+            "sizes": "48x48",
+            "type": "image/png"
+          },
+          {
+            "src": "icon-72.png",
+            "sizes": "72x72",
+            "type": "image/png"
+          },
+          {
+            "src": "icon-96.png",
+            "sizes": "96x96",
+            "type": "image/png"
+          },
+          {
+            "src": "icon-128.png",
+            "sizes": "128x128",
+            "type": "image/png"
+          },
+          {
+            "src": "icon-144.png",
+            "sizes": "144x144",
+            "type": "image/png"
+          },
+          {
+            "src": "icon-152.png",
+            "sizes": "152x152",
+            "type": "image/png"
+          },
+          {
+            "src": "icon-192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "icon-384.png",
+            "sizes": "384x384",
+            "type": "image/png"
+          },
+          {
+            "src": "icon-512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          }
+        ]        
+      }
+    })
   ],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
